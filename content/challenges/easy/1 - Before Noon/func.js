d9e4cda6b11f68e14b54cccc9ca8211fa249e107
Forrest Payne <djkurosu@gmail.com>
2020-04-27 09:10:00 -0400

    attempts at 1-5-easy

--- content/challenges/easy/1 - Before Noon/func.js
+++ content/challenges/easy/1 - Before Noon/func.js
@@ -1,5 +1,7 @@
 // Check if the time is before noon
 // time is in military time 0-24
 module.exports = (hour) => {
-    return hour < 12;
+    if (hour < 12) {
+    return "Before Noon";
+    }
 }
--- content/challenges/easy/3 - Movies/func.js
+++ content/challenges/easy/3 - Movies/func.js
@@ -6,4 +6,13 @@
 
 // Return True or False in every situation
 module.exports = (customerMoney, isMovieFull, rating, age, isParentWith) => {
+    if (isMovieFull || (customerMoney < 8)) {
+        return "cannot buy";
+    } else if (rating == "PG") {
+        return "can buy";
+    } else if ((age >= 17) || (isParentWith)) {
+        return "can buy"
+    } else {
+        return "cannot buy"
+    }
 };
